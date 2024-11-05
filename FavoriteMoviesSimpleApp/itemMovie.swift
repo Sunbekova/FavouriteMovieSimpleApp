47d623da169aaafc5db979b6867918a185bf0486
Sunbekova <suanbekova05@gmail.com>
2024-11-05 23:24:45 +0500

    Create MovieList (file)

---
+++ FavoriteMoviesSimpleApp/itemMovie.swift
@@ -0,0 +1,16 @@
+//
+//  ItemMovieList.swift
+//  FavoriteMoviesSimpleApp
+//
+//  Created by Aisha Suanbekova Bakytjankyzy on 05.11.2024.
+//
+
+import Foundation
+import UIKit
+
+struct Movie {
+    let name: String
+    let year: Int
+    let image: UIImage
+    let genre: String
+}
---
+++ FavoriteMoviesSimpleApp/itemMovieList.swift
@@ -0,0 +1,62 @@
+//
+//  ItemMovieList.swift
+//  FavoriteMoviesSimpleApp
+//
+//  Created by Aisha Suanbekova Bakytjankyzy on 05.11.2024.
+//
+
+import Foundation
+import UIKit
+
+let movies: [Movie] = [
+    Movie(name: "Gentlemen Prefer Blondes",
+          year: 1953,
+          image: .gentlemen,
+          genre: "Musical Comedy"
+         ),
+    Movie(name: "The Prince of Egypt",
+          year: 1998,
+          image: .prince,
+          genre: "Musical Drama"
+         ),
+    Movie(name: "Silent Voice",
+          year: 2016,
+          image: .voice,
+          genre: "Drama"
+         ),
+    Movie(name: "Warriors of the Steppe - Myn Bala",
+          year: 2012,
+          image: .mynBala,
+          genre: "Historical Drama"
+         ),
+    Movie(name: "Tomris",
+          year: 2019,
+          image: .tomris,
+          genre: "Historical Thriler"
+         ),
+    Movie(name: "Kurmanjan Datka",
+          year: 2014,
+          image: .курманжанДаткаТасмасы,
+          genre: "Historical Drama"
+         ),
+    Movie(name: "Qazaq Handygy. Altyn Taq",
+          year: 2019,
+          image: .altynTaq,
+          genre: "Historical Thriler"
+         ),
+    Movie(name: "Code Geass Lelouch of the Re;surrection",
+          year: 2019,
+          image: .codeGeassmovieposter,
+          genre: "Science Fantasy"
+         ),
+    Movie(name: "Barbie",
+          year: 2023,
+          image: .barbie,
+          genre: "Fantasy Comedy"
+         ),
+    Movie(name: "Made in Abyss: Dawn of the Deep Soul",
+          year: 2020,
+          image: .madeInAbyssDawnOfTheDeepSoul,
+          genre: "Dark Fantasy"
+         )
+]
