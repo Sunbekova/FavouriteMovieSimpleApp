c5bf047335314b7f90367f83eda1a71c18a87c23
Sunbekova <suanbekova05@gmail.com>
2024-11-07 00:36:46 +0500

    Version of app for 7.11.24 (not finished)

--- FavoriteMoviesSimpleApp/MovieDetailsPage.swift
+++ FavoriteMoviesSimpleApp/MovieDetailsPage.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+class MovieDetailsPage: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let movie = inputModel {
+            movieTitleinPage.text = movie.name
+            movieImageinPage.image = movie.image
+            movieYearinPage.text = String(movie.year)
+            movieGenreinPage.text = movie.genre
+            movieDescription.text = movie.description
+        } else {
+            movieTitleinPage.text = "No movie details available"
+            movieImageinPage.image = UIImage(named: "defaultImage") // Optional default image
+            movieYearinPage.text = "N/A"
+            movieGenreinPage.text = "N/A"
+            movieDescription.text = "Description not available"
+                }
+    }
+    @IBOutlet weak var movieTitleinPage: UILabel!
+    @IBOutlet weak var movieGenreinPage: UILabel!
+    @IBOutlet weak var movieYearinPage: UILabel!
+    @IBOutlet weak var movieDescription: UITextView!
+    @IBOutlet weak var movieImageinPage: UIImageView!
+    private var inputModel: Movie?
+    func configure(with model: Movie?){
+        self.inputModel = model
+    }
+}
--- FavoriteMoviesSimpleApp/ViewController.swift
+++ FavoriteMoviesSimpleApp/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private let movieList = movies
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        uiTableView.dataSource = self
+        uiTableView.delegate = self
+        uiTableView.register(MovieTableViewCell.nib(), forCellReuseIdentifier: MovieTableViewCell.identifier)
     }
 
-
+    @IBOutlet weak var uiTableView: UITableView!
+    
 }
 
+extension ViewController: UITableViewDataSource, UITableViewDelegate{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movieList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: MovieTableViewCell.identifier, for: indexPath) as! MovieTableViewCell
+        
+        let currentMovie = movieList[indexPath.row]
+        cell.configure(with: currentMovie)
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            // Get the selected movie
+            let selectedMovie = movieList[indexPath.row]
+            
+            // Instantiate the MovieDetailsPage view controller
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            if let detailedVC = storyboard.instantiateViewController(withIdentifier: "movieDetailsVC") as? MovieDetailsPage {
+                
+                // Pass the selected movie to the MovieDetailsPage
+                detailedVC.configure(with: selectedMovie)
+                
+                // Push the MovieDetailsPage onto the navigation stack
+                navigationController?.pushViewController(detailedVC, animated: true)
+            }
+        }
+}
