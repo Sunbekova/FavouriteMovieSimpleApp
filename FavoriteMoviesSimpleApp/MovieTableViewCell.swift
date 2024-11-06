11ddfc136682fb5179cf3e2801258fed0fae0f18
Sunbekova <suanbekova05@gmail.com>
2024-11-07 00:35:23 +0500

    create tableView cell

--- FavoriteMoviesSimpleApp/MovieTableViewCell.swift
+++ FavoriteMoviesSimpleApp/MovieTableViewCell.swift
@@ -8,6 +8,22 @@
 import UIKit
 
 class MovieTableViewCell: UITableViewCell {
+    
+    static let identifier = "MovieTableViewCell"
+    
+    @IBOutlet weak var movieImageCell: UIImageView!
+    @IBOutlet weak var movieTitleCell: UILabel!
+    @IBOutlet weak var movieGenreCell: UILabel!
+    
+    
+    private var inputModel: Movie?
+    func configure(with model: Movie?){
+        self.inputModel = model
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: "MovieTableViewCell", bundle: nil)
+    }
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -17,7 +33,9 @@ class MovieTableViewCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
+        movieImageCell.image = inputModel?.image
+        movieTitleCell.text = inputModel?.name
+        movieGenreCell.text = inputModel?.genre
     }
     
 }
